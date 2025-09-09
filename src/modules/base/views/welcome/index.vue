e9b1f63c505894d766ea619cf50e120135fdaf31
daxiong <babyseeme@qq.com>
2025-09-10 00:35:20 +0800

    feat(welcome): 添加接口文档链接并支持多语言
    - 在 .env.development 文件中添加 VITE_APP_DOC_URL 环境变量
    - 在英文和中文语言文件中添加 API 文档相关翻译
    - 在欢迎页面添加接口文档按钮，点击后根据当前语言环境打开相应文档
    - 优化欢迎页面布局，实现按钮垂直居中

--- src/modules/base/views/welcome/index.vue
+++ src/modules/base/views/welcome/index.vue
@@ -8,19 +8,34 @@
  - @Link   https://github.com/mineadmin
 -->
 <script setup lang="tsx">
-import WorkbenchFast from "./components/workbench/workbench-fast.vue";
-import WorkbenchLogin from "~/base/views/welcome/components/workbench/workbench-login.vue";
-
+import WorkbenchFast from './components/workbench/workbench-fast.vue'
+import WorkbenchLogin from '~/base/views/welcome/components/workbench/workbench-login.vue'
+import { useI18n } from 'vue-i18n'
+import { el } from 'element-plus/es/locale/index.mjs'
 
 defineOptions({ name: 'welcome' })
 const userinfo = useUserStore().getUserInfo()
+const i18n = useTrans() as TransType
+const t = i18n.globalTrans
+const documentUrl = import.meta.env.VITE_APP_DOC_URL
 
+const { locale } = useI18n()
+function openDocument() {
+  // 判断当前语言环境
+  console.log('locale', locale.value)
+  if (locale.value === 'en') {
+    window.open(`${documentUrl}/en`, '_blank')
+  }
+  else {
+    window.open(documentUrl, '_blank')
+  }
+}
 </script>
 
 <template>
   <div class="mine-layout">
     <div class="flex justify-between bg-white p-3 dark-bg-dark-8">
-      <div class="flex gap-x-5">
+      <div class="w-full flex gap-x-5">
         <el-avatar :src="userinfo?.avatar" :size="80">
           <span v-if="!userinfo?.avatar" class="text-5xl">{{ userinfo.username[0].toUpperCase() }}</span>
         </el-avatar>
@@ -28,6 +43,12 @@ const userinfo = useUserStore().getUserInfo()
           <span class="text-xl">早安，{{
             userinfo.username
           }}！</span>
+        </div>
+        <!-- 将 ml-auto 的 div 移出 flex 容器，并使用 flex items-center 实现垂直居中 -->
+        <div class="ml-auto mr-5 flex items-center">
+          <el-button type="primary" @click="openDocument">
+            {{ t('welcome.document') }}
+          </el-button>
         </div>
       </div>
     </div>
